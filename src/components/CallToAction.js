f7c423c62ea982621b53fd4977728ff8acd637cd
Robert Wise <robcodehub@gmail.com>
2020-11-22 06:22:24 +1100

    updated links to chrome extension

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import Button from "./Button";
+import React from "react"
+import Button from "./Button"
 
 const CallToAction = props => {
-
   return (
     <section className="cta">
       <h1>Install the Chrome Extension</h1>
       <p>Install the Chrome extension on the Chrome Store or using GitHub</p>
-      <a href="https://github.com/oslabs-beta/ApolloDevQL" target="_blank" rel="noreferrer"> <Button aria-label="Chrome Extension" label="Install the Chrome Extension" /></a>
-      <a href="https://github.com/oslabs-beta/ApolloDevQL" target="_blank" rel="noreferrer"><Button aria-label="GitHub Code" label="View the GitHub Code" /></a>
+      <a
+        href="https://chrome.google.com/webstore/detail/kdbhdgkakklkjhcfiighgonefimkpaeh"
+        target="_blank"
+        rel="noreferrer"
+      >
+        {" "}
+        <Button
+          aria-label="Chrome Extension"
+          label="Install the Chrome Extension"
+        />
+      </a>
+      <a
+        href="https://github.com/oslabs-beta/ApolloDevQL"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Button aria-label="GitHub Code" label="View the GitHub Code" />
+      </a>
     </section>
   )
-};
+}
 
-export default CallToAction;
+export default CallToAction
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,21 @@
-import { Link } from "gatsby";
-import PropTypes from "prop-types";
-import React, { useState } from 'react';
-import Backdrop from './BackdropSection';
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React, { useState } from "react"
+import Backdrop from "./BackdropSection"
 
 const Header = ({ siteTitle }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const toggle = () => setIsOpen(!isOpen);
+  const [isOpen, setIsOpen] = useState(false)
+  const toggle = () => setIsOpen(!isOpen)
 
-  let navClasses = 'nav-links';
-  let navItemClasses = 'nav-link-item';
-  let burgerClass = "burger";
-  let backdropClass = "backdrop";
+  let navClasses = "nav-links"
+  let navItemClasses = "nav-link-item"
+  let burgerClass = "burger"
+  let backdropClass = "backdrop"
   if (isOpen) {
-    navClasses = 'nav-links nav-active';
-    navItemClasses = 'nav-link-item-active';
-    burgerClass = "burger toggle";
-    backdropClass = "backdrop backdrop-active";
-
+    navClasses = "nav-links nav-active"
+    navItemClasses = "nav-link-item-active"
+    burgerClass = "burger toggle"
+    backdropClass = "backdrop backdrop-active"
   }
 
   return (
@@ -26,17 +25,37 @@ const Header = ({ siteTitle }) => {
         <div className="nav-belt">
           <div>
             <h4>
-              <Link className="logo" to="/" >
+              <Link className="logo" to="/">
                 <div className="logo-image"></div>
                 {siteTitle}
               </Link>
             </h4>
           </div>
           <ul className={navClasses}>
-            <li className={navItemClasses}><a href="#features">Features</a></li>
-            <li className={navItemClasses}><a href="#team">Team</a></li>
-            <li className={navItemClasses}><a href="https://github.com/oslabs-beta/ApolloDevQL" target="_blank" rel="noreferrer">GitHub</a></li>
-            <li className={`${navItemClasses} nav-item-bold`} ><a href="https://github.com/oslabs-beta/ApolloDevQL" target="_blank" rel="noreferrer">Install Extension</a></li>
+            <li className={navItemClasses}>
+              <a href="#features">Features</a>
+            </li>
+            <li className={navItemClasses}>
+              <a href="#team">Team</a>
+            </li>
+            <li className={navItemClasses}>
+              <a
+                href="https://github.com/oslabs-beta/ApolloDevQL"
+                target="_blank"
+                rel="noreferrer"
+              >
+                GitHub
+              </a>
+            </li>
+            <li className={`${navItemClasses} nav-item-bold`}>
+              <a
+                href="https://chrome.google.com/webstore/detail/kdbhdgkakklkjhcfiighgonefimkpaeh"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Install Extension
+              </a>
+            </li>
           </ul>
           <div role="navigation" className={burgerClass} onClick={toggle}>
             <div className="burger-line-1"></div>
@@ -47,7 +66,7 @@ const Header = ({ siteTitle }) => {
       </nav>
     </header>
   )
-};
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
@@ -57,4 +76,4 @@ Header.defaultProps = {
   siteTitle: ``,
 }
 
-export default Header;
+export default Header
